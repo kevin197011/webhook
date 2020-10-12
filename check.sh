d922d659928a03ce28daaebabec244fd980d1ad9
Kevin197011 <kevin197011@outlook.com>
2020-10-12 19:35:35 +0800

    Update.

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "test whatsapp..."
-curl --location --request POST 'http://localhost:8000/whatsapp' \
+curl --location --request POST 'http://localhost:8000/v1/whatsapp' \
 --header 'Content-type: application/json' \
 --data @./testdata/post.json
 echo ""
