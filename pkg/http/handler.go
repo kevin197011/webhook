2c72399ca03f9bdb3a0d4b1acd165f51206b882d
kevin197011 <61411931+kevin197011@users.noreply.github.com>
2020-12-21 16:04:18 +0800

    Update handler.go

--- pkg/http/handler.go
+++ pkg/http/handler.go
@@ -201,3 +201,65 @@ func whatsappAlertsHandlerB(c *gin.Context) {
 		"data": "success!",
 	})
 }
+
+
+func whatsappAlertsHandlerC(c *gin.Context) {
+	var messages HookMessage
+	var emoji string
+
+	if err := c.Bind(&messages); err != nil {
+		zap.L().Error("Bind json data fail", zap.Error(err))
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code": http.StatusInternalServerError,
+			"msg":  "bad",
+			"data": "failed!",
+		})
+		return
+	}
+	zap.L().Info("Bind json data success", zap.Any("data", messages))
+
+	if messages.Status == "firing" {
+		emoji = "❌"
+	} else {
+		emoji = "✅"
+	}
+
+	for _, alert := range messages.Alerts {
+		var msg string
+		msg = fmt.Sprintf("*状态:* %s %s\\n", messages.Status, emoji)
+		msg += "*标签:*\\n"
+		keys := make([]string, 0, len(alert.Labels))
+		for k := range alert.Labels {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		for _, key := range keys {
+			msg += fmt.Sprintf("  %s = %s\\n", key, alert.Labels[key])
+		}
+		if message, ok := alert.Annotations["message"]; ok {
+			msg += fmt.Sprintf("*注解:*\\n  %s\\n", message)
+		} else {
+			msg += fmt.Sprintf("*注解:*\\n  %s\\n", alert.Annotations["summary"])
+		}
+		timeVal, err := utils.TimeFormat(alert.StartsAt)
+		if err != nil {
+			zap.L().Error("Time format error!", zap.Error(err))
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"code": http.StatusInternalServerError,
+				"msg":  "bad",
+				"data": "failed!",
+			})
+			return
+		}
+		msg += fmt.Sprintf("*时间:* %s", timeVal)
+		zap.L().Info("send alert data.", zap.Any("msg", msg))
+		providers.SendMsg(providers.NewWhatsappOpt("123456", config.NewConfig().GroupNameC), msg)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": http.StatusOK,
+		"msg":  "ok",
+		"data": "success!",
+	})
+}
+
